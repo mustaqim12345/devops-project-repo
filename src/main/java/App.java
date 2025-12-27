e24bf8b3c1c9fd60a9afe6cf04919de2e5c3efd4
root <root@first-project-vm.asia-east1-a.c.fiery-booth-459706-s8.internal>
2025-12-27 06:51:46 +0000

    auto build test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("HELLO MY NAME IS HUMAIRA KHAN  THIS IS MY FIRST PROJECT Hello DevOps AUTO BUILD TEST
-");
+        System.out.println("HELLO MY NAME IS HUMAIRA KHAN Hello DevOps AUTO BUILD TEST ");
         while(true){}
     }
 }
