d9d7becffd63832615dead383252ff72c02a39e5
root <root@first-project-vm.asia-east1-a.c.fiery-booth-459706-s8.internal>
2025-12-27 06:26:54 +0000

    test pipeline

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Java App Running Successfully");
+        System.out.println("HELLO MY NAME IS HUMAIRA KHAN  THIS IS MY FIRST PROJECT");
         while(true){}
     }
 }
