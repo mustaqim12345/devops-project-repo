ad6d03cb203953de81142c6d5d2ee996b1bdedad
root <root@jenkins-devops-vm.us-central1-b.c.kkgcplabs01-024.internal>
2026-01-18 18:04:40 +0000

    Add Spring Boot web application

---
+++ src/main/java/com/company/Application.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @GetMapping("/")
+    public String home() {
+        return "CI/CD Pipeline Working Successfully 🚀";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
