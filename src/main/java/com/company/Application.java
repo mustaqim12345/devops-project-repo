fbb0df1dc7c10ddf8d83e678fc668d02f842218f
mustaqim12345 <mustaqim8859@gmail.com>
2026-01-19 00:06:05 +0530

    Rename home method to hello and update response

--- src/main/java/com/company/Application.java
+++ src/main/java/com/company/Application.java
@@ -10,12 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
 
     @GetMapping("/")
-    public String home() {
-        return "CI/CD Pipeline Working Successfully 🚀";
+    public String hello() {
+        return "Hello from Docker + Jenkins + GCP 🚀";
     }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
-
