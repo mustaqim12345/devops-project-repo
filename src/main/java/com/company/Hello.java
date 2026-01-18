b1a0233f2a7eabb60ddfdfc264d05b9bfff49739
root <root@jenkins-devops-vm.us-central1-b.c.kkgcplabs01-024.internal>
2026-01-18 18:30:58 +0000

    Fix executable jar with Main-Class

---
+++ src/main/java/com/company/Hello.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("CI/CD App Running on GCP 🚀");
+        while(true) {}   // app ko alive rakhne ke liye
+    }
+}
